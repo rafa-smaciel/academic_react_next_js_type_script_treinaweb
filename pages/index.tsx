c50a920621b423016cef5db22c8d2a5a4403ce89
rafael <rafaelmdeveloper@gmail.com>
2022-10-05 20:47:42 -0300

    props next.js

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+
+function Botao(props){
+  return <a href="#">{props.titulo}</a>
+}
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Botao titulo="Click" /> <br />
+      <Botao titulo="Entrar" /> <br />
+      <Botao titulo="Ver" /> <br />
+      <Botao titulo="Ver Mais" /> <br />
+    </div>
+  )
+}
+
+export default Home
